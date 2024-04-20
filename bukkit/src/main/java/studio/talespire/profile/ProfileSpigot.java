9704cf80787ebef0888a408e002413db1278541d
ogilcher <olivergilcher@gmail.com>
2024-04-19 20:11:34 -0700

    feat: file uploads

---
+++ bukkit/src/main/java/studio/talespire/profile/ProfileSpigot.java
@@ -0,0 +1,17 @@
+package studio.talespire.profile;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ProfileSpigot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
