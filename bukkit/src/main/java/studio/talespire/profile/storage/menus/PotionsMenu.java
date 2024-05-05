9fdf08a5a3e2315c1c43031ce007affdfdee7349
ogilcher <olivergilcher@gmail.com>
2024-05-05 11:00:11 -0700

    feat: Potions Button

--- bukkit/src/main/java/studio/talespire/profile/storage/menus/PotionsMenu.java
+++ bukkit/src/main/java/studio/talespire/profile/storage/menus/PotionsMenu.java
@@ -1,4 +1,6 @@
 package studio.talespire.profile.storage.menus;
 
-public class PotionsMenu {
+import studio.lunarlabs.universe.menus.api.Menu;
+
+public class PotionsMenu extends Menu {
 }
---
+++ bukkit/src/main/java/studio/talespire/profile/storage/menus/buttons/PotionsButton.java
@@ -0,0 +1,37 @@
+package studio.talespire.profile.storage.menus.buttons;
+
+import lombok.RequiredArgsConstructor;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.inventory.ItemStack;
+import studio.lunarlabs.universe.Universe;
+import studio.lunarlabs.universe.menus.api.Button;
+import studio.lunarlabs.universe.menus.api.MenuHandler;
+import studio.lunarlabs.universe.util.ItemBuilder;
+import studio.lunarlabs.universe.util.XSound;
+import studio.talespire.profile.storage.menus.PotionsMenu;
+
+@RequiredArgsConstructor
+public class PotionsButton extends Button {
+    private final boolean isSelected;
+
+    @Override
+    public ItemStack getItem(Player player) {
+        return new ItemBuilder(Material.POTION)
+                .setName(isSelected? ChatColor.GREEN + "Potions" : ChatColor.GRAY +  "Potions")
+                .toItemStack();
+    }
+
+    @Override
+    public void clicked(Player player, ClickType clickType) {
+        if (isSelected) {
+            player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 1, 1);
+        } else {
+            player.playSound(player.getLocation(), XSound.BLOCK_NOTE_BLOCK_HARP.parseSound(), 20f, 0.1f);
+            Universe.get(MenuHandler.class).openMenuAsync(new PotionsMenu(), player);
+        }
+    }
+}
