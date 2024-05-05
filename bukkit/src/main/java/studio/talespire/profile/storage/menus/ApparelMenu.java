d61bdbfa67f71ad57233f6e1061edfeb22fccf6f
ogilcher <olivergilcher@gmail.com>
2024-05-05 10:55:33 -0700

    feat: Apparel Button

--- bukkit/src/main/java/studio/talespire/profile/storage/menus/ApparelMenu.java
+++ bukkit/src/main/java/studio/talespire/profile/storage/menus/ApparelMenu.java
@@ -1,4 +1,6 @@
 package studio.talespire.profile.storage.menus;
 
-public class ApparelMenu {
+import studio.lunarlabs.universe.menus.api.Menu;
+
+public class ApparelMenu extends Menu {
 }
---
+++ bukkit/src/main/java/studio/talespire/profile/storage/menus/buttons/ApparelButton.java
@@ -0,0 +1,35 @@
+package studio.talespire.profile.storage.menus.buttons;
+
+import lombok.RequiredArgsConstructor;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.inventory.ItemStack;
+import studio.lunarlabs.universe.Universe;
+import studio.lunarlabs.universe.menus.api.Button;
+import studio.lunarlabs.universe.menus.api.MenuHandler;
+import studio.lunarlabs.universe.util.ItemBuilder;
+import studio.talespire.profile.storage.menus.ApparelMenu;
+
+@RequiredArgsConstructor
+public class ApparelButton extends Button {
+    private final boolean isSelected;
+
+    @Override
+    public ItemStack getItem(Player player) {
+        return new ItemBuilder(Material.NETHERITE_CHESTPLATE)
+            .setName(isSelected? ChatColor.GREEN + "Apparel" : ChatColor.GRAY + "Apparel")
+            .toItemStack();
+    }
+
+    @Override
+    public void clicked(Player player, ClickType clickType) {
+        if (isSelected) {
+            player.playSound(player.getLocation(), org.bukkit.Sound.ENTITY_VILLAGER_NO, 1, 1);
+        } else {
+            player.playSound(player.getLocation(), org.bukkit.Sound.BLOCK_NOTE_BLOCK_HARP, 20f, 0.1f);
+            Universe.get(MenuHandler.class).openMenuAsync(new ApparelMenu(), player);
+        }
+    }
+}
